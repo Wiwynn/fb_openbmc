57146a4416983b61562e219fa66353489327c689
Kapil Hali <kapilhali@fb.com>
2016-08-15 16:19:40 -0700

    Add crash dump script for platform FBTP
    Summary:
    Add dump.sh script for providing manual crash dump for
    platform FBTP - coreid, tor and msr
    
    Test Plan: Tested on platform FBTP
    
    Reviewers: tfang, jmehta, siumingtong, mattconolly, reed, sdasari
    
    Reviewed By: sdasari
    
    Subscribers: net-systems-diffs@
    
    Differential Revision: https://phabricator.intern.facebook.com/D3718794
    
    Signature: t1:3718794:1471301777:954b995413c4849c099664f67775a35e59a50266

--- meta-facebook/meta-fbtp/recipes-fbtp/private/files/dump.sh
+++ meta-facebook/meta-fbtp/recipes-fbtp/private/files/dump.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-max_proc_id=31
-max_mc_idx=19
-max_retry=0
+max_proc_id=35
+max_mc_idx=20
+max_retry=4
 ME_COLD_RESET_DELAY=5;
 
 ME_UTIL="/usr/local/bin/me-util"
@@ -13,7 +13,6 @@ ME_UTIL_LOG="/tmp/me-util.log"
 #echo "whole parameter  ==> $#"
 #echo "parameter 1      ==> $1"
 #echo "parameter 2      ==> $2"
-#echo "parameter 3      ==> $3"
 
 if [ $1 = "time" ]; then
   now=$(date)
@@ -23,23 +22,29 @@ fi
 
 echo ""
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 2 ]; then
 
-  echo "$0 <slot#> 48 coreid  ==> for CPU 1 CoreID"
-	echo "$0 <slot#> 48 tor     ==> for CPU 1 TOR"
-	echo "$0 <slot#> 48 msr     ==> for CPU 1 MSR"
-
-	exit 1
-
-elif [ "$2" -ne 48 ]; then
-
-	echo "$0  <slot#> 48 coreid  ==> for CPU 1 CoreID"
-	echo "$0  <slot#> 48 tor     ==> for CPU 1 TOR"
-	echo "$0  <slot#> 48 msr     ==> for CPU 1 MSR"
+	echo "$0 48 coreid  ==> for CPU 1 CoreID"
+	echo "$0 49 coreid  ==> for CPU 2 CoreID"
+	echo "$0 48 tor     ==> for CPU 1 TOR"
+	echo "$0 49 tor     ==> for CPU 2 TOR"
+	echo "$0 48 msr     ==> for CPU 1 MSR"
+	echo "$0 49 msr     ==> for CPU 2 MSR"
 
 	exit 1
 
-elif [ "$3" = "coreid" ]; then
+elif [ "$1" -ne 48 ] && [ "$1" -ne 49 ]; then
+
+	echo "$0  48 coreid  ==> for CPU 1 CoreID"
+	echo "$0  49 coreid  ==> for CPU 2 CoreID"
+	echo "$0  48 tor     ==> for CPU 1 TOR"
+	echo "$0  49 tor     ==> for CPU 2 TOR"
+	echo "$0  48 msr     ==> for CPU 1 MSR"
+	echo "$0  49 msr     ==> for CPU 2 MSR"
+
+	exit 1
+
+elif [ "$2" = "coreid" ]; then
 
   echo ""
   echo "CPU COREID DUMP:"
@@ -48,82 +53,100 @@ elif [ "$3" = "coreid" ]; then
 
 	#PECI RdPkgConfig() "CPUID Read"
 	echo "< CPUID Read >"
-  cmdout=$($ME_UTIL $1 0xB8 0x40 0x57 0x01 0x00 0x30 0x05 0x05 0xa1 0x00 0x00 0x00 0x00)
+  cmdout=$($ME_UTIL 0xB8 0x40 0x57 0x01 0x00 $1 0x05 0x05 0xa1 0x00 0x00 0x00 0x00)
   head -n 1 $ME_UTIL_LOG
 
 	#PECI RdPkgConfig() "CPU Microcode Update Revision Read"
 	echo "< CPU Microcode Update Revision Read >"
-  cmdout=$($ME_UTIL $1 0xB8 0x40 0x57 0x01 0x00 0x30 0x05 0x05 0xa1 0x00 0x00 0x04 0x00)
+  cmdout=$($ME_UTIL 0xB8 0x40 0x57 0x01 0x00 $1 0x05 0x05 0xa1 0x00 0x00 0x04 0x00)
   head -n 1 $ME_UTIL_LOG
 
 	#PECI RdPkgConfig() "MCA ERROR SOURCE LOG Read"
 	echo "< MCA ERROR SOURCE LOG Read -- The socket which MCA_ERR_SRC_LOG[30]=0 is the socket that asserted IERR first >"
-  cmdout=$($ME_UTIL $1 0xB8 0x40 0x57 0x01 0x00 0x30 0x05 0x05 0xa1 0x00 0x00 0x05 0x00)
+  cmdout=$($ME_UTIL 0xB8 0x40 0x57 0x01 0x00 $1 0x05 0x05 0xa1 0x00 0x00 0x05 0x00)
   head -n 1 $ME_UTIL_LOG
 
 	#PECI RdPkgConfig() "Core ID IERR"
 	#echo "< Core ID IERR -- determine whether a core in the failing socket asserted an IERR, completion data[3]=1 if a core caused the IERR, data[2:0] is the core ID, and save the value for matching purpose >"
 	#ipmitool -H $1 -U $user -P $passwd -b 6 -t 0x2c raw 0x2E 0x40 0x57 0x01 0x00 $2 0x05 0x05 0xa1 0x00 0x27 0x08 0x08
 
-	if [ "$2" -eq 48 ]; then
+	if [ "$1" -eq 48 ]; then
 
 		printf "********************************************************\n"
 		printf "*                   IERRLOGGINGREG                     *\n"
 		printf "********************************************************\n"
 
-    cmdout=$($ME_UTIL $1 0xB8 0x44 0x57 0x01 0x00 0x40 0xA4 0x50 0x18 0x00 0x03)
+    cmdout=$($ME_UTIL 0xB8 0x44 0x57 0x01 0x00 0x40 0xA4 0x50 0x18 0x00 0x03)
     head -n 1 $ME_UTIL_LOG
 
 		printf "********************************************************\n"
 		printf "*                   MCERRLOGGINGREG                    *\n"
 		printf "********************************************************\n"
 
-    cmdout=$($ME_UTIL $1 0xB8 0x44 0x57 0x01 0x00 0x40 0xA8 0x50 0x18 0x00 0x03)
+    cmdout=$($ME_UTIL 0xB8 0x44 0x57 0x01 0x00 0x40 0xA8 0x50 0x18 0x00 0x03)
+    head -n 1 $ME_UTIL_LOG
+
+	elif [ "$1" -eq 49 ]; then
+
+		printf "********************************************************\n"
+		printf "*                   IERRLOGGINGREG                     *\n"
+		printf "********************************************************\n"
+
+    cmdout=$($ME_UTIL 0xB8 0x44 0x57 0x01 0x00 0x41 0xA4 0x50 0x18 0x00 0x03)
+    head -n 1 $ME_UTIL_LOG
+
+		printf "********************************************************\n"
+		printf "*                   MCERRLOGGINGREG                    *\n"
+		printf "********************************************************\n"
+
+    cmdout=$($ME_UTIL 0xB8 0x44 0x57 0x01 0x00 0x41 0xA8 0x50 0x18 0x00 0x03)
     head -n 1 $ME_UTIL_LOG
 
 	fi
 
 	exit 1
 
-#elif [ "$3" = "tor" ]; then
-#	CboIndex=0
-#	TorArrayIndex=0
-#	param=0	
-#	param0=0
-#	param1=0
-#	BankNumber=0
-#	wfc=186
-#	rfc=72
-#
-#	while [ "$CboIndex" -le 13 ]
-#	do
-#		TorArrayIndex=0
-#		while [ "$TorArrayIndex" -le 19 ]
-#		do
-#			BankNumber=0
-#			while [ "$BankNumber" -le 2 ]
-#			do
-#				param=$(($BankNumber + $(($TorArrayIndex << 2)) + $(($CboIndex << 7))))
-#				param0=$(($param & 255))
-#				param1=$(($param >> 8))
-#				#echo "< CboIndex=$CboIndex, TorArrayIndex=$TorArrayIndex, BankNumber=$BankNumber Read >"
-#				#echo "$BankNumber $TorArrayIndex $CboIndex $2 05 05 a1 00 27 $param"
-#				printf "%.2x %.2x %.2x %.2x 05 05 a1 00 27 %.4x %.2x" $BankNumber $TorArrayIndex $CboIndex $2 $param $wfc
-#				ipmitool -H $1 -U $user -P $passwd -b 6 -t 0x2c raw 0x2E 0x40 0x57 0x01 0x00 $2 0x05 0x05 0xa1 0x00 0x27 $param0 $param1 
-#				#printf " %.2x\n" $rfc
-#
-#				BankNumber=$(($BankNumber+1))
-#			done
-#
-#			TorArrayIndex=$(($TorArrayIndex+1))
-#		done
-#
-#		CboIndex=$(($CboIndex+1))
-#	done
-#
-#	exit 1
+elif [ "$2" = "tor" ]; then
+	CboIndex=0
+	TorArrayIndex=0
+	param=0
+	param0=0
+	param1=0
+	BankNumber=0
+	wfc=186
+	rfc=72
 
-elif [ "$3" = "msr" ]; then
+	while [ "$CboIndex" -le 13 ]
+	do
+		TorArrayIndex=0
+		while [ "$TorArrayIndex" -le 19 ]
+		do
+			BankNumber=0
+			while [ "$BankNumber" -le 2 ]
+			do
+				param=$(($BankNumber + $(($TorArrayIndex << 2)) + $(($CboIndex << 7))))
+				param0=$(($param & 255))
+				param1=$(($param >> 8))
+				#echo "< CboIndex=$CboIndex, TorArrayIndex=$TorArrayIndex, BankNumber=$BankNumber Read >"
+				#echo "$BankNumber $TorArrayIndex $CboIndex $1 05 05 a1 00 27 $param"
+				#printf "%.2x %.2x %.2x %.2x 05 05 a1 00 27 %.4x %.2x" $BankNumber $TorArrayIndex $CboIndex $1 $param $wfc
+				#ipmitool -H -U $user -P $passwd -b 6 -t 0x2c raw 0x2E 0x40 0x57 0x01 0x00 $2 0x05 0x05 0xa1 0x00 0x27 $param0 $param1
+				cmdout=$($ME_UTIL 0xB8 0x40 0x57 0x01 0x00 $1 0x05 0x05 0xa1 0x00 0x27 $param0 $param1)
+				head -n 1 $ME_UTIL_LOG
+				#printf " %.2x\n" $rfc
+
+				BankNumber=$(($BankNumber+1))
+			done
+
+			TorArrayIndex=$(($TorArrayIndex+1))
+		done
+
+		CboIndex=$(($CboIndex+1))
+	done
+
+	exit 1
+
+elif [ "$2" = "msr" ]; then
 
 	MC_index=0
 	MSR_offset=0
@@ -155,7 +178,7 @@ elif [ "$3" = "msr" ]; then
 		printf "********************************************************\n"
 
 		printf "   <<< IA32_MC%u_CTL, ProcessorID from 0 to %u >>> \n" $MC_index $max_proc_id
- 
+
 		ProcessorID=0
 		retry=0
 		while [ "$ProcessorID" -le $max_proc_id ]
@@ -167,7 +190,7 @@ elif [ "$3" = "msr" ]; then
 			#printf "[issue] %.2x %.4x %.2x %.2x %.2x %.2x %s %s\n" $MC_index $MSR_offset $ProcessorID $param $param0 $param1 $user $passwd
 			#printf "[issue] ProcessorID=%u MSR_Address=0x%.4x IA32_MC%u_CTL MSR_offset=0x%.4x\n" $ProcessorID $param $MC_index $MSR_offset
 			#printf "[issue] ipmitool -H %s -U %s -P %s -b 6 -t 0x2c raw 0x2E 0x40 0x57 0x01 0x00 0x%.2x 0x05 0x09 0xb1 0x00 0x%.2x 0x%.2x 0x%.2x\n" $1 $user $passwd $2  $ProcessorID $param0 $param1
-			cmdout=$($ME_UTIL $1 0xB8 0x40 0x57 0x01 0x00 0x30 0x05 0x09 0xb1 0x00 $ProcessorID $param0 $param1)
+			cmdout=$($ME_UTIL 0xB8 0x40 0x57 0x01 0x00 $1 0x05 0x09 0xb1 0x00 $ProcessorID $param0 $param1)
       response=$(head -n 1 $ME_UTIL_LOG)
 			comp=$?
 			return=$(echo $response | cut -d ' ' -f 4)
@@ -198,7 +221,7 @@ elif [ "$3" = "msr" ]; then
 			#printf "[issue] %.2x %.2x %.2x %.2x %.2x %s %s\n" $MC_index $ProcessorID $param $param0 $param1 $user $passwd
 			#printf "[issue] ProcessorID=%u MSR_Address=0x%.4x IA32_MC%u_CTL2 MC_index=0x%.4x\n" $ProcessorID $param $MC_index $MC_index
 			#printf "[issue] ipmitool -H %s -U %s -P %s -b 6 -t 0x2c raw 0x2E 0x40 0x57 0x01 0x00 0x%.2x 0x05 0x09 0xb1 0x00 0x%.2x 0x%.2x 0x%.2x\n" $1 $user $passwd $2  $ProcessorID $param0 $param1
-			cmdout=$($ME_UTIL $1 0xB8 0x40 0x57 0x01 0x00 0x30 0x05 0x09 0xb1 0x00 $ProcessorID $param0 $param1)
+			cmdout=$($ME_UTIL 0xB8 0x40 0x57 0x01 0x00 $1 0x05 0x09 0xb1 0x00 $ProcessorID $param0 $param1)
       response=$(head -n 1 $ME_UTIL_LOG)
 			comp=$?
 			return=$(echo $response | cut -d ' ' -f 4)
@@ -229,7 +252,7 @@ elif [ "$3" = "msr" ]; then
 			#printf "[issue] %.2x %.4x %.2x %.2x %.2x %.2x %s %s\n" $MC_index $MSR_offset $ProcessorID $param $param0 $param1 $user $passwd
 			#printf "[issue] ProcessorID=%u MSR_Address=0x%.4x IA32_MC%u_STATUS MSR_offset=0x%.4x\n" $ProcessorID $param $MC_index $MSR_offset
 			#printf "[issue] ipmitool -H %s -U %s -P %s -b 6 -t 0x2c raw 0x2E 0x40 0x57 0x01 0x00 0x%.2x 0x05 0x09 0xb1 0x00 0x%.2x 0x%.2x 0x%.2x\n" $1 $user $passwd $2  $ProcessorID $param0 $param1
-			cmdout=$($ME_UTIL $1 0xB8 0x40 0x57 0x01 0x00 0x30 0x05 0x09 0xb1 0x00 $ProcessorID $param0 $param1)
+			cmdout=$($ME_UTIL 0xB8 0x40 0x57 0x01 0x00 $1 0x05 0x09 0xb1 0x00 $ProcessorID $param0 $param1)
       response=$(head -n 1 $ME_UTIL_LOG)
 			comp=$?
 			return=$(echo $response | cut -d ' ' -f 4)
@@ -260,7 +283,7 @@ elif [ "$3" = "msr" ]; then
 			#printf "[issue] %.2x %.4x %.2x %.2x %.2x %.2x %s %s\n" $MC_index $MSR_offset $ProcessorID $param $param0 $param1 $user $passwd
 			#printf "[issue] ProcessorID=%u MSR_Address=0x%.4x IA32_MC%u_ADDR MSR_offset=0x%.4x\n" $ProcessorID $param $MC_index $MSR_offset
 			#printf "[issue] ipmitool -H %s -U %s -P %s -b 6 -t 0x2c raw 0x2E 0x40 0x57 0x01 0x00 0x%.2x 0x05 0x09 0xb1 0x00 0x%.2x 0x%.2x 0x%.2x\n" $1 $user $passwd $2  $ProcessorID $param0 $param1
-			cmdout=$($ME_UTIL $1 0xB8 0x40 0x57 0x01 0x00 0x30 0x05 0x09 0xb1 0x00 $ProcessorID $param0 $param1)
+			cmdout=$($ME_UTIL 0xB8 0x40 0x57 0x01 0x00 $1 0x05 0x09 0xb1 0x00 $ProcessorID $param0 $param1)
       response=$(head -n 1 $ME_UTIL_LOG)
 			comp=$?
 			return=$(echo $response | cut -d ' ' -f 4)
@@ -282,7 +305,7 @@ elif [ "$3" = "msr" ]; then
 
 		ProcessorID=0
 		retry=0
-		while [ "$ProcessorID" -le $max_proc_id ] 
+		while [ "$ProcessorID" -le $max_proc_id ]
 		do
 			param=$(($IA32_MC_MISC_base + $MSR_offset))
 			param0=$(($param & 255))
@@ -291,7 +314,7 @@ elif [ "$3" = "msr" ]; then
 			#printf "[issue] %.2x %.4x %.2x %.2x %.2x %.2x %s %s\n" $MC_index $MSR_offset $ProcessorID $param $param0 $param1 $user $passwd
 			#printf "[issue] ProcessorID=%u MSR_Address=0x%.4x IA32_MC%u_MISC MSR_offset=0x%.4x\n" $ProcessorID $param $MC_index $MSR_offset
 			#printf "[issue] ipmitool -H %s -U %s -P %s -b 6 -t 0x2c raw 0x2E 0x40 0x57 0x01 0x00 0x%.2x 0x05 0x09 0xb1 0x00 0x%.2x 0x%.2x 0x%.2x\n" $1 $user $passwd $2  $ProcessorID $param0 $param1
-			cmdout=$($ME_UTIL $1 0xB8 0x40 0x57 0x01 0x00 0x30 0x05 0x09 0xb1 0x00 $ProcessorID $param0 $param1)
+			cmdout=$($ME_UTIL 0xB8 0x40 0x57 0x01 0x00 $1 0x05 0x09 0xb1 0x00 $ProcessorID $param0 $param1)
       response=$(head -n 1 $ME_UTIL_LOG)
 			comp=$?
 			return=$(echo $response | cut -d ' ' -f 4)
@@ -365,7 +388,7 @@ elif [ "$3" = "msr" ]; then
 		#printf "[issue] %.2x %.2x %.2x %.2x %s %s\n" $ProcessorID $param $param0 $param1 $user $passwd
 		#printf "[issue] ProcessorID=%u MSR_Address=0x%.4x IA32_MCG_STATUS\n" $ProcessorID $param
 		#printf "[issue] ipmitool -H %s -U %s -P %s -b 6 -t 0x2c raw 0x2E 0x40 0x57 0x01 0x00 0x%.2x 0x05 0x09 0xb1 0x00 0x%.2x 0x%.2x 0x%.2x\n" $1 $user $passwd $2  $ProcessorID $param0 $param1
-		cmdout=$($ME_UTIL $1 0xB8 0x40 0x57 0x01 0x00 0x30 0x05 0x09 0xb1 0x00 $ProcessorID $param0 $param1)
+		cmdout=$($ME_UTIL 0xB8 0x40 0x57 0x01 0x00 $1 0x05 0x09 0xb1 0x00 $ProcessorID $param0 $param1)
     response=$(head -n 1 $ME_UTIL_LOG)
 		comp=$?
 		return=$(echo $response | cut -d ' ' -f 4)
