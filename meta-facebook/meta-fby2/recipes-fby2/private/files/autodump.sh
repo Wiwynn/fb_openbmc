f729d4b82d02193b44a2b3fe865253fd1af2e4eb
Ben Wei <benwei@fb.com>
2018-02-22 16:11:48 -0800

    fby2: add sensor snapshot to crashdump
    Summary:
    - when crash dump is triggered, append sensor history for that host (from previous 3 min)
      as well as sensor threshold to log file
    - tar zip logfile at end of operation
    
    Test Plan:
    verified on YosemiteV2 system by manually trigger autodump
    
    root@bmc-oob:~# autodump.sh slot3
    Auto Dump for slot3 Started
    Auto Dump for slot3 Completed
    root@bmc-oob:~# ls /mnt/data
    crashdump_slot3.tar.gz  etc                     kv_store                ppr                     sel2.bin                sel4.bin
    cri_sel                 hostname                logfile                 sel1.bin                sel3.bin
    
    root@bmc-oob:~# zcat /mnt/data/crashdump_slot3.tar.gz
    crashdump_slot30000644000000000000000000045710213243716147012632 0ustar  rootrootCrash Dump generated at Thu Feb 22 20:45:04 PST 2018
    <snip>
    Sensor history at dump:
    
    MB Outlet Temp_T   (0x7) min = 33.00, average = 33.00, max = 33.00
    MB Outlet Temp_B   (0xD) min = 30.00, average = 30.00, max = 30.00
    MB Inlet Temp      (0x1) min = 27.00, average = 27.00, max = 27.00
    PCH Temp           (0x8) min = 33.00, average = 34.45, max = 35.00
    SOC Temp           (0x5) min = 28.00, average = 29.17, max = 30.00
    SOC Therm Margin   (0x9) min = -60.00, average = -58.83, max = -58.00
    SOC Tjmax          (0x30) min = 88.00, average = 88.00, max = 88.00
    SOC DIMMA0 Temp    (0xB4) min = 30.00, average = 30.08, max = 31.00
    <snip>
    Sensor threshold at dump:
    
    MB Outlet Temp_T             (0x7) :   31.00 C     | (ok) | UCR: 93.00 | UNC: NA | UNR: NA | LCR: NA | LNC: NA | LNR: NA
    MB Outlet Temp_B             (0xD) :   30.00 C     | (ok) | UCR: 93.00 | UNC: NA | UNR: NA | LCR: NA | LNC: NA | LNR: NA
    MB Inlet Temp                (0x1) :   27.00 C     | (ok) | UCR: 93.00 | UNC: NA | UNR: NA | LCR: NA | LNC: NA | LNR: NA
    PCH Temp                     (0x8) :   32.00 C     | (ok) | UCR: 93.00 | UNC: NA | UNR: NA | LCR: NA | LNC: NA | LNR: NA
    SOC Temp                     (0x5) :   27.00 C     | (ok) | UCR: 84.00 | UNC: NA | UNR: NA | LCR: NA | LNC: NA | LNR: NA
    SOC Therm Margin             (0x9) :  -61.00 C     | (ok) | UCR: 0.00 | UNC: NA | UNR: NA | LCR: NA | LNC: NA | LNR: NA
    
    Reviewed By: amithash
    
    Differential Revision: D7059295
    
    fbshipit-source-id: 619d10f

--- meta-facebook/meta-fby2/recipes-fby2/private/files/autodump.sh
+++ meta-facebook/meta-fby2/recipes-fby2/private/files/autodump.sh
@@ -31,7 +31,7 @@ PID_FILE="/var/run/autodump$SLOT_NUM.pid"
 if [ -f $PID_FILE ]; then
   echo "Another auto crashdump for $SLOT_NAME is runnung"
   exit 1
-else 
+else
   touch $PID_FILE
 fi
 
@@ -42,6 +42,9 @@ echo $((sys_runtime+630)) > /tmp/cache_store/fru${SLOT_NUM}_crashdump
 
 DUMP_SCRIPT="/usr/local/bin/dump.sh"
 CRASHDUMP_FILE="/mnt/data/crashdump_$SLOT_NAME"
+CRASHDUMP_LOG_ARCHIVE="/mnt/data/crashdump_$SLOT_NAME.tar.gz"
+
+echo "Auto Dump for $SLOT_NAME Started"
 
 #HEADER LINE for the dump
 $DUMP_SCRIPT "time" > $CRASHDUMP_FILE
@@ -49,7 +52,17 @@ $DUMP_SCRIPT "time" > $CRASHDUMP_FILE
 $DUMP_SCRIPT $SLOT_NAME "coreid" >> $CRASHDUMP_FILE
 #MSR dump
 $DUMP_SCRIPT $SLOT_NAME "msr" >> $CRASHDUMP_FILE
+# Sensors & sensor thresholds
+echo "Sensor history at dump:" >> $CRASHDUMP_FILE 2>&1
+$DUMP_SCRIPT $SLOT_NAME "sensors" >> $CRASHDUMP_FILE
+echo "Sensor threshold at dump:" >> $CRASHDUMP_FILE 2>&1
+$DUMP_SCRIPT $SLOT_NAME "threshold" >> $CRASHDUMP_FILE
+
+tar zcf $CRASHDUMP_LOG_ARCHIVE -C `dirname $CRASHDUMP_FILE` `basename $CRASHDUMP_FILE` && \
+rm -rf $CRASHDUMP_FILE && \
+logger -t "ipmid" -p daemon.crit "Crashdump for $SLOT_NAME is generated at $CRASHDUMP_LOG_ARCHIVE"
+
+echo "Auto Dump for $SLOT_NAME Completed"
 
 rm $PID_FILE
 exit 0
-
--- meta-facebook/meta-fby2/recipes-fby2/private/files/dump.sh
+++ meta-facebook/meta-fby2/recipes-fby2/private/files/dump.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ME_UTIL="/usr/local/bin/me-util"
+SENSOR_HISTORY=180
 
 if [ "$#" -eq 1 ] && [ $1 = "time" ]; then
   now=$(date)
@@ -9,8 +10,10 @@ if [ "$#" -eq 1 ] && [ $1 = "time" ]; then
 fi
 
 if [ "$#" -ne 2 ]; then
-  echo "$0 <slot#> coreid  ==> for CPU CoreID"
-  echo "$0 <slot#> msr     ==> for CPU MSR"
+  echo "$0 <slot#> coreid    ==> for CPU CoreID"
+  echo "$0 <slot#> msr       ==> for CPU MSR"
+  echo "$0 <slot#> sensors   ==> for sensor history"
+  echo "$0 <slot#> threshold ==> for sensor threshold"
   exit 1
 fi
 
@@ -20,6 +23,12 @@ if [ "$2" = "coreid" ]; then
   [ -r /usr/local/fbpackages/me-util/crashdump_coreid ] && $ME_UTIL $1 --file /usr/local/fbpackages/me-util/crashdump_coreid
 elif [ "$2" = "msr" ]; then
   [ -r /usr/local/fbpackages/me-util/crashdump_msr ] && $ME_UTIL $1 --file /usr/local/fbpackages/me-util/crashdump_msr
+elif [ "$2" = "sensors" ]; then
+  /usr/local/bin/sensor-util $1 --history $SENSOR_HISTORY \
+   && /usr/local/bin/sensor-util spb --history $SENSOR_HISTORY \
+   && /usr/local/bin/sensor-util nic --history $SENSOR_HISTORY
+elif [ "$2" = "threshold" ]; then
+  /usr/local/bin/sensor-util all --threshold 
 fi
 
 exit 0
