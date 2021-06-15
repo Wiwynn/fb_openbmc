e5106a72311ceb002698ac2e2c8505c6eaac4ec7
Mark Kuo <Mark_kuo@wiwynn.com>
2021-06-14 21:15:25 -0700

    fby35: initial commit (#1627)
    Summary:
    - initialize fby35 platform
    - implement BB CPLD Update
    - modify the power-on.sh for register
    - enable BMC jtag1 interface and include jbi in image, in order that fw-util can update BMC CPLD FW via jtag when input file is jbc.
    - fix fan lower pwm issue and add yv35 into openbmc-init-build-env
    - add init-ifupdown in recipes-core
    - add support MLX multi-host
    - implement AST BIC firmware update
    - modify the way of sled cycle from BMC to CPLD
    - modify the setting of AST2600 register
    - implement sb cpld update and sb componet f/w reading
    - implement bios firmware update and sb gpio related features
    - implement bic firmware recovery update
    - modify the FM_PWRBRK_X_R/direction gpio direction
    - Fix the issue fruid-util help message is wrong and might not be able to read/write fru
    - detect the slot fru
    - modify usb setting for AST1030
    - implement BIOS update via USB to SPI
    - disable the action of controlling SB USB HUB
    - modify clear cmos via bic-util based on poc2
    - modify adc sensors and gpio for poc2
    - implement read Renesas vr verison
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.wiwynn/pull/1627
    
    Test Plan: -Build code:pass
    
    Reviewed By: zhdaniel12
    
    Differential Revision: D29033422
    
    Pulled By: benwei13
    
    fbshipit-source-id: e994e4c601

---
+++ meta-facebook/meta-fby35/recipes-proprietary/acd/files/PlatInfo.cpp
@@ -0,0 +1,162 @@
+/******************************************************************************
+ *
+ * INTEL CONFIDENTIAL
+ *
+ * Copyright 2019 Intel Corporation.
+ *
+ * This software and the related documents are Intel copyrighted materials,
+ * and your use of them is governed by the express license under which they
+ * were provided to you ("License"). Unless the License provides otherwise,
+ * you may not use, modify, copy, publish, distribute, disclose or transmit
+ * this software or the related documents without Intel's prior written
+ * permission.
+ *
+ * This software and the related documents are provided as is, with no express
+ * or implied warranties, other than those that are expressly stated in the
+ * License.
+ *
+ ******************************************************************************/
+
+extern "C" {
+#include <stdio.h>
+}
+#include <string>
+#include <sstream>
+
+#include "CrashdumpSections/MetaData.hpp"
+
+using namespace std;
+
+extern int node_bus_id;
+
+void split(const std::string& s, std::vector<std::string>& sv, const char delim = ' ') {
+    sv.clear();
+    std::istringstream iss(s);
+    std::string temp;
+
+    while (std::getline(iss, temp, delim)) {
+        sv.emplace_back(std::move(&temp[2]));
+    }
+
+    return;
+}
+
+static int get_fw_ver(string fru, string comp, string &ver) {
+  char line[128];
+  FILE *fp;
+  string cmd;
+
+  cmd = "/usr/bin/fw-util " + fru + " --version " + comp;
+  if (!(fp = popen(cmd.c_str(), "r")))
+    return -1;
+
+  do {
+    if (!fgets(line, sizeof(line), fp))
+      break;
+
+    string str = line;
+    auto start = str.find(":");
+    if (start == string::npos)
+      break;
+
+    start = str.find_first_not_of(" ", start+1);
+    if (start == string::npos)
+      break;
+
+    auto end = str.find("\n", start);
+    if (end == string::npos) {
+      end = str.size();
+    }
+
+    ver = str.substr(start, end - start);
+  } while (0);
+  pclose(fp);
+
+  return 0;
+}
+
+int getBmcVersion(char *ver) {
+  string ver_str;
+
+  if (get_fw_ver("bmc", "bmc", ver_str)) {
+    return -1;
+  }
+
+  cd_snprintf_s(ver, SI_JSON_STRING_LEN, "%s", ver_str.c_str());
+  return 0;
+}
+
+int getBiosVersion(char *ver) {
+  string ver_str;
+  string slot_str = "slot" + to_string(node_bus_id+1);
+
+  if (get_fw_ver(slot_str, "bios", ver_str)) {
+    return -1;
+  }
+
+  cd_snprintf_s(ver, SI_JSON_STRING_LEN, "%s", ver_str.c_str());
+  return 0;
+}
+
+int getMeVersion(char *ver) {
+  string ver_str;
+  string slot_str = "slot" + to_string(node_bus_id+1);
+
+  if (get_fw_ver(slot_str, "me", ver_str)) {
+    return -1;
+  }
+
+  cd_snprintf_s(ver, SI_JSON_STRING_LEN, "%s", ver_str.c_str());
+  return 0;
+}
+
+int getSystemGuid(string &guid) {
+  char line[128], cmd[128];
+  FILE *fp;
+  string slot_str = "slot" + to_string(node_bus_id+1);
+  string guid_str("");
+
+  snprintf(cmd, sizeof(cmd), "/usr/local/bin/guid-util %s sys --get 2>/dev/null", slot_str.c_str());
+
+  if (!(fp = popen(cmd, "r")))
+    return -1;
+
+  do {
+    if (!fgets(line, sizeof(line), fp))
+      break;
+
+    string str = line;
+    auto start = str[0];
+    if (start == string::npos)
+      break;
+
+    size_t pos = 0;
+    while (( pos = str.find ("\n",pos)) != string::npos ) {
+      str.erase (pos, 1);
+    }
+
+    std::vector<std::string> sv;
+    split(str, sv, ':');
+
+    // Hanle LSB
+    std::swap(sv[0],sv[3]);
+    std::swap(sv[1],sv[2]);
+    std::swap(sv[4],sv[5]);
+    std::swap(sv[6],sv[7]);
+    for (int i = 0; i < (int)sv.size(); i++ ) {
+      if ( i == 4 || i == 6 || i == 8 || i == 10 ) {
+        guid_str += "-";
+      }
+      guid_str += sv[i];
+    }
+
+    guid = guid_str;
+  } while (0);
+  pclose(fp);
+
+  return 0;
+}
+
+void platformInit(uint8_t fru) {
+  node_bus_id = fru - 1;
+}
