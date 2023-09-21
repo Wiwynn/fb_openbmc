fd6799e6c56aa31c168551011c624214544cdcfd
Allen.Wang <Allen_Wang@quantatw.com>
2023-09-20 20:02:23 -0700

    halfdome: proprietary: addc: trigger S0 Cold Reset by PWRBUTTON_L
    Summary:
    # Description
    According to ADDC Whitepaper, AMD prefer perform S0 Cold Reset by PWRBUTTON_L signal(like DC cycle)
    
    # Motivation
    MP1 status will keep after S0 Cold Reset and BIOS able to parse errors informations from dmesg.
    
    X-link: https://github.com/facebookexternal/openbmc.quanta/pull/4087
    
    Test Plan:
    Build/Test on halfdome
    
    Inject MP1 UCE error:
    sudo ./cmdline_amdraserrorinjectiontest --smupsp --socket 0 --errormode 1 --enablemcapolling 0 --errorindex 0 --errortype 1
    
    ADDC trigger cycle automatically and BIOS add aMACHINE_CHK_ERR after system reboot:
    
    1    slot1    2023-09-19 18:05:25    ipmid            SEL Entry: FRU: 1, Record: Standard (0x02), Time: 2023-09-19 18:05:25, Sensor: SYSTEM_STATUS (0x10), Event Data: (2004FF) Alert_L RAS_STATUS(0x04), Control fabric error Assertion
    1    slot1    2023-09-19 18:05:25    ipmid            Crashdump for FRU: 1 started
    1    slot1    2023-09-19 18:05:26    amd-ras          Dump completed, cold reset FRU: 1
    1    slot1    2023-09-19 18:05:33    gpiod            FRU: 1, System powered OFF
    1    slot1    2023-09-19 18:05:38    ipmid            Crashdump for FRU: 1 is generated at /mnt/data/fru1_ras-error0_20230919-180525.tar.gz
    1    slot1    2023-09-19 18:05:40    gpiod            FRU: 1, System powered ON
    1    slot1    2023-09-19 18:06:23    ipmid            SEL Entry: FRU: 1, Record: Standard (0x02), Time: 2023-09-19 18:06:23, Sensor: PSB_STS (0x46), Event Data: (EE00FF) PSB Pass Assertion
    1    slot1    2023-09-19 18:06:34    ipmid            SEL Entry: FRU: 1, Record: Standard (0x02), Time: 2023-09-19 18:06:34, Sensor: MACHINE_CHK_ERR (0x40), Event Data: (AB5D00) Uncorrected System Fatal Error, Bank Number 29 (SMU/MPDMA), CPU 0, CCD 0 Assertion
    
    Reviewed By: amithash
    
    Differential Revision: D49443618
    
    fbshipit-source-id: 79fb6c841f7815d85af6915eafa623ae378c6879

--- meta-facebook/meta-fby35/meta-halfdome/recipes-proprietary/addc/files/src/plat.cpp
+++ meta-facebook/meta-fby35/meta-halfdome/recipes-proprietary/addc/files/src/plat.cpp
@@ -7,6 +7,20 @@
 
 extern uint8_t fru;
 
+constexpr int DELAY_POWER_OFF = 6000;
+constexpr int DELAY_POWER_CYCLE = 5000;
+constexpr int DELAY_POWER_ON = 1000;
+
+static inline void triggerPowerButton(const int durationMs) {
+    const std::string pwr_ctl = "/usr/bin/bic-util slot" +
+        std::to_string(fru) + " 0x18 0x52 0x00 0x42 0x00 0x00 ";
+
+    std::system((pwr_ctl + "0xff").c_str());
+    std::system((pwr_ctl + "0xfe").c_str());
+    std::this_thread::sleep_for(std::chrono::milliseconds(durationMs));
+    std::system((pwr_ctl + "0xff").c_str());
+}
+
 void set_apml_mux_locked(uint8_t cpu) {
     (void)cpu;
 }
@@ -20,13 +34,10 @@ void apml_mux_unlock(uint8_t cpu) {
 }
 
 void triggerColdReset(void) {
-    const std::string pwr_ctl = "/usr/bin/bic-util slot" +
-        std::to_string(fru) + " 0x18 0x52 0x00 0x42 0x00 0x00 ";
-
     syslog(LOG_CRIT, "Dump completed, cold reset FRU: %u", fru);
-    // toggle reset button
-    std::system((pwr_ctl + "0xff").c_str());
-    std::system((pwr_ctl + "0xfd").c_str());
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    std::system((pwr_ctl + "0xff").c_str());
+
+    //ADDC Whitepaper : S0 Cold Reset trigger by PWRBUTTON_L
+    triggerPowerButton(DELAY_POWER_OFF);
+    std::this_thread::sleep_for(std::chrono::milliseconds(DELAY_POWER_CYCLE));
+    triggerPowerButton(DELAY_POWER_ON);
 }
