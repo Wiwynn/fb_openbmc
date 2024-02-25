4f383e88631fc619a94c8f1056267c987915553f
Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
2024-02-25 12:01:44 +0800

    yosemite4: set mctp i3c link up
    Change-Id: If656fc123f7c7f4c8bd46de763bec3fc95df0395

--- meta-facebook/meta-yosemite4/recipes-networking/mctp/files/mctp-config.sh
+++ meta-facebook/meta-yosemite4/recipes-networking/mctp/files/mctp-config.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-# Set mctpi2c link up and assign local address.
 localEid=8
 
-# The Sentinel Dome BICs are on the i2c bus0 to bus7.
+# Set mctpi3c link up and assign local address.
+# The Sentinel Dome BICs are after the I3C hub on I3C bus0 and bus1.
 busnum=0
-while [ $busnum -le 7 ]
+while [ $busnum -le 1 ]
 do
-    mctp link set mctpi2c${busnum} up
-    mctp addr add ${localEid} dev mctpi2c${busnum}
+    mctp link set mctpi3c${busnum} up
+    mctp addr add ${localEid} dev mctpi3c${busnum}
     busnum=$((busnum+1))
 done
 
+# Set mctpi2c link up and assign local address.
 # The NICs are on the i2c bus24 to bus27.
 busnum=24
 while [ $busnum -le 27 ]
